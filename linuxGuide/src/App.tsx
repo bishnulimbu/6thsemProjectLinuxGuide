190c6ec99fc31d71fdf49b2d8e4bd315e66d271d
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-24 16:06:21 +0545

    main app file

---
+++ linuxGuide/src/App.tsx
@@ -0,0 +1,14 @@
+import AppRoutes from "./routes/AppRoutes";
+import "./styles/global.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+};
+
+export default App;
