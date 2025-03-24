66908c86680fdeaf21c952d406a41d8ce4fb994c
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-24 16:05:52 +0545

    ui component for card

---
+++ linuxGuide/src/components/ui/FeatureCard.tsx
@@ -0,0 +1,19 @@
+import { FeatureCardProps } from "../../types";
+import "./FeatureCard.css";
+
+// Define the FeatureCard component
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  title,
+  description,
+  icon,
+}) => {
+  return (
+    <div className="feature-card">
+      <span className="feature-icon">{icon}</span>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default FeatureCard;
