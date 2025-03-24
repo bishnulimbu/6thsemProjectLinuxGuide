fdb07144ed3a9e7cf3b4fec538b9bddbbe459d00
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-24 16:04:59 +0545

    user model
    removed another table role as replaced roles with enum

---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("super_admin", "admin", "user"),
+      allowNull: false,
+      defaultValue: "user",
+    },
+  },
+  { tableName: "users" },
+);
+
+module.exports = User;
