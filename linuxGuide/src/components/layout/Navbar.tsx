26171ef851e1d877d0ea27ff65c3604a536cbac8
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-24 16:05:44 +0545

    navbar

---
+++ linuxGuide/src/components/layout/Navbar.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h1>DemoApp</h1>
+      </div>
+      <button className="nav-toggle" onClick={toggleNav}>
+        {isNavOpen ? "✖" : "☰"}
+      </button>
+      <ul className={`nav-links ${isNavOpen ? "open" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
