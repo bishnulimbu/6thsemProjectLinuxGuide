9548899a6b2baad0ae90cc3a72aac06c1d1b6730
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-24 18:46:43 +0545

    guide model

---
+++ backend/models/Guide.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Guide = sequelize.define(
+  "Guide",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("draft", "published"),
+      allowNull: false,
+      defaultValue: "draft",
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "guides",
+  },
+);
+
+module.exports = Guide;
