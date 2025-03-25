2fd3d4956003e2da994e607f4846f4669636e808
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-25 18:08:53 +0545

    added relation probably

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Guide = require("./Guide");
+const Comment = require("./Comment");
 
 User.hasMany(Post, { foreignKey: "userId" });
 Post.belongsTo(User, { foreignKey: "userId" });
@@ -8,4 +9,13 @@ Post.belongsTo(User, { foreignKey: "userId" });
 User.hasMany(Guide, { foreignKey: "userId" });
 Guide.belongsTo(User, { foreignKey: "userId" });
 
-module.exports = { User, Post, Guide };
+User.hasMany(Comment, { foreignKey: "userId" });
+Comment.belongsTo(User, { foreignKey: "userId" });
+
+Guide.hasMany(Comment, { foreignKey: "guideId" });
+Comment.belongsTo(Guide, { foreignKey: "guideId" });
+
+Post.hasMany(Comment, { foreignKey: "postId" });
+Comment.belongsTo(Post, { foreignKey: "postId" });
+
+module.exports = { User, Post, Guide, Comment };
