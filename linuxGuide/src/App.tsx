95be58d47d2e32f08d3d391127746b5d7c8ac5ed
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-26 20:01:02 +0545

    wrap App component with AuthProvider and add Navbar, Footer, and ToastContainer

--- linuxGuide/src/App.tsx
+++ linuxGuide/src/App.tsx
@@ -1,11 +1,23 @@
+import React from "react";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import { Link } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles/global.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 const App = () => {
   return (
-    <div className="App">
-      <AppRoutes />
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Navbar />
+        <AppRoutes />
+        <Footer />
+        <ToastContainer />
+      </div>
+    </AuthProvider>
   );
 };
 
