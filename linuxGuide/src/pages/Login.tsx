5d3177eb7ceb218d1a1cdff7ddf841488a05e8ae
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-27 10:38:13 +0545

    remove unused CSS import from Login component

--- linuxGuide/src/pages/Login.tsx
+++ linuxGuide/src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
