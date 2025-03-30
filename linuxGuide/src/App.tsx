d5adae2bed52ddc14a867e54f720f71bd2557f85
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-30 16:41:07 +0545

    refactor: adjust App component layout for improved width handling

--- linuxGuide/src/App.tsx
+++ linuxGuide/src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div className="max-w-6xl mx-auto p-5 min-h-screen flex flex-col">
+        <div className="max-w-full mx-auto p-5 min-h-screen flex flex-col">
           <Navbar />
           <AppRoutes />
           <Footer />
