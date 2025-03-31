2314f2efebc07ef8c83cdd02098d2c80b6f53938
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-31 18:40:51 +0545

    feat: update routing for posts and guides, add PostDetail and Admin components

--- linuxGuide/src/routes/AppRoutes.tsx
+++ linuxGuide/src/routes/AppRoutes.tsx
@@ -3,13 +3,14 @@ import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import Post from "../pages/Post";
+import PostDetail from "../pages/PostDetail";
 import Guide from "../pages/Guide";
 import GuideDetail from "../pages/GuideDetail";
 import CreatePost from "../pages/CreatePost";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
-// import Admin from '../pages/ad' will need a admin page.
+import Admin from "../pages/Admin";
 
 const AppRoutes: React.FC = () => (
   <main>
@@ -17,12 +18,14 @@ const AppRoutes: React.FC = () => (
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/post" element={<Post />} />
-      <Route path="/guide" element={<Guide />} />
-      <Route path="/guide/:id" element={<GuideDetail />} />
+      <Route path="/posts" element={<Post />} />
+      <Route path="/guides" element={<Guide />} />
+      <Route path="/guides/:id" element={<GuideDetail />} />
+      <Route path="/posts/:id" element={<PostDetail />} />
       <Route path="/create-post" element={<CreatePost />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/admin" element={<Admin />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </main>
