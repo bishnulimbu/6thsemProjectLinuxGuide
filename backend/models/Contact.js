e8ce72e6327b4292604d253033c913027975f17b
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-31 18:35:16 +0545

    feat: add Contact model for managing contact information

---
+++ backend/models/Contact.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Contact = sequelize.define(
+  "Contact",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "contacts",
+  },
+);
+
+module.exports = Contact;
