6e44311d4bbe902acbfd6f37e433bac94ee8feb0
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-31 18:35:22 +0545

    feat: add Tag and PostTag models for improved tagging functionality

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,9 @@ const User = require("./User");
 const Post = require("./Post");
 const Guide = require("./Guide");
 const Comment = require("./Comment");
+const Contact = require("./Contact");
+const Tag = require("./Tag");
+const PostTag = require("./PostTag");
 
 User.hasMany(Post, { foreignKey: "userId" });
 Post.belongsTo(User, { foreignKey: "userId" });
@@ -18,4 +21,13 @@ Comment.belongsTo(Guide, { foreignKey: "guideId" });
 Post.hasMany(Comment, { foreignKey: "postId" });
 Comment.belongsTo(Post, { foreignKey: "postId" });
 
-module.exports = { User, Post, Guide, Comment };
+Post.hasMany(PostTag, { foreignKey: "postId" });
+PostTag.belongsTo(Post, { foreignKey: "postId" });
+
+Tag.hasMany(PostTag, { foreignKey: "tagId" });
+PostTag.belongsTo(Tag, { foreignKey: "tagId" });
+
+Post.belongsToMany(Tag, { through: PostTag, foreignKey: "postId" });
+Tag.belongsToMany(Post, { through: PostTag, foreignKey: "tagId" });
+
+module.exports = { User, Post, Guide, Comment, Contact, Tag, PostTag };
