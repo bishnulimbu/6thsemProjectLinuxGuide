4272e5955bbc3209e8c7d9b994fb215c0fe23df3
bishnulimbu <subashlimbu987654321@gmail.com>
2025-03-31 18:35:35 +0545

    feat: add Tag model for managing tags in the database

---
+++ backend/models/Tag.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Tag = sequelize.define(
+  "Tag",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    tableName: "tags",
+    timestamps: false,
+  },
+);
+
+module.exports = Tag;
